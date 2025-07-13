9e4fd3491f1ef85a32ccbe8dedd5754f868f85e0
Alex Maldonado <alex@oasci.org>
2025-07-13 08:25:47 -0400

    pixi run format

--- tests/managers/test_task_chunks.py
+++ tests/managers/test_task_chunks.py
@@ -59,4 +59,3 @@ def test_task_generator_large_chunk(dummy_task_manager):
     chunks = list(dummy_task_manager.task_generator(items, chunk_size))
     assert len(chunks) == 1
     assert chunks[0] == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-
